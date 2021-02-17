a35450e20087c708498b5d43ffd3f50011841c79
Shouryee <79190892+Shouryee@users.noreply.github.com>
2021-02-17 14:15:31 +0530

    Add files via upload

---
+++ main.css
@@ -0,0 +1,51 @@
+body{
+    margin:0;
+    padding: 800px;
+    text-align: center;
+    }
+.profile{
+    border-radius: 80%;
+}
+#welcome{
+    position: absolute;
+    height:100%;
+    color: white;
+    bottom: 0%;
+     background:linear-gradient(#bfffcd 0%, #07f73c 100%);
+    display: table;
+    width: 100%;
+    z-index:9;
+}
+.menu{
+    position: fixed;
+    background: white;
+    width: 100%;
+    margin-top: 0px;
+    top: 0px;
+    z-index: 10;
+    
+}
+.links{
+    display: inline;
+    padding-left: 33px;
+    padding-right: 25px;
+    font-family: fantasy;
+    font-size: 20px;
+}
+.links:hover{
+    font-size: 25px;
+    text-decoration: underline;
+}
+#contact{
+    background: firebrick;
+}
+h1{
+    font-size: 40px;
+    font-family: sans-serif;
+    font-weight: 300;
+    
+}
+p{
+    font-size: 17px;
+    
+}
